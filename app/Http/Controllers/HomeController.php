eca0d5a96d34eac6eb3e41c64dd00673b29b6a62
reytama86 <120538889+reytama86@users.noreply.github.com>
2024-10-03 08:10:29 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -63,18 +63,18 @@ class HomeController extends Controller
         return view('home.product', compact('product', 'latest_products'));
     }
 
-    // public function product_search(Request $request)
-    // {
-    //     // Gunakan $products sesuai kebutuhan dalam pencarian
-    //     $productName = $request->input('name');
-    //     $searchedProducts = Product::where('name', 'like', "%$productName%")->get();
+    public function product_search(Request $request)
+    {
+        // Gunakan $products sesuai kebutuhan dalam pencarian
+        $productName = $request->input('name');
+        $searchedProducts = Product::where('name', 'like', "%$productName%")->get();
 
-    //     return response()->json(['products' => $searchedProducts]);
-    // }
-    // public function showSearchForm()
-    // {
-    //     return view('home.search'); // Sesuaikan dengan nama file view Anda
-    // }
+        return response()->json(['products' => $searchedProducts]);
+    }
+    public function showSearchForm()
+    {
+        return view('home.search'); // Sesuaikan dengan nama file view Anda
+    }
     public function cart()
     {
         if (!Auth::guard('webmember')->user()) {
@@ -85,65 +85,65 @@ class HomeController extends Controller
         return view('home.cart', compact('carts','cart_total'));
     }
 
-    // public function get_kota($id)
-    // {
-    //     $curl = curl_init();
+    public function get_kota($id)
+    {
+        $curl = curl_init();
 
-    //     curl_setopt_array($curl, array(
-    //         CURLOPT_URL => "https://api.rajaongkir.com/starter/city?province=" . $id,
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_ENCODING => "",
-    //         CURLOPT_MAXREDIRS => 10,
-    //         CURLOPT_TIMEOUT => 30,
-    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //         CURLOPT_CUSTOMREQUEST => "GET",
-    //         CURLOPT_HTTPHEADER => array(
-    //             "key: 1adb8efa384245ff3bc71d4d2429518a"
-    //         ),
-    //     ));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/city?province=" . $id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "key: 1adb8efa384245ff3bc71d4d2429518a"
+            ),
+        ));
 
-    //     $response = curl_exec($curl);
-    //     $err = curl_error($curl);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-    //     curl_close($curl);
+        curl_close($curl);
 
-    //     if ($err) {
-    //         echo "cURL Error #:" . $err;
-    //     } else {
-    //         echo $response;
-    //     }
-    // }
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+    }
 
-    // public function get_ongkir($destination, $weight)
-    // {
-    //     $curl = curl_init();
+    public function get_ongkir($destination, $weight)
+    {
+        $curl = curl_init();
 
-    //     curl_setopt_array($curl, array(
-    //         CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_ENCODING => "",
-    //         CURLOPT_MAXREDIRS => 10,
-    //         CURLOPT_TIMEOUT => 30,
-    //         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    //         CURLOPT_CUSTOMREQUEST => "POST",
-    //         CURLOPT_POSTFIELDS => "origin=369&destination=" . $destination . "&weight=" . $weight . "&courier=jne",
-    //         CURLOPT_HTTPHEADER => array(
-    //             "content-type: application/x-www-form-urlencoded",
-    //             "key: 1adb8efa384245ff3bc71d4d2429518a"
-    //         ),
-    //     ));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/cost",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "origin=369&destination=" . $destination . "&weight=" . $weight . "&courier=jne",
+            CURLOPT_HTTPHEADER => array(
+                "content-type: application/x-www-form-urlencoded",
+                "key: 1adb8efa384245ff3bc71d4d2429518a"
+            ),
+        ));
 
-    //     $response = curl_exec($curl);
-    //     $err = curl_error($curl);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-    //     curl_close($curl);
+        curl_close($curl);
 
-    //     if ($err) {
-    //         echo "cURL Error #:" . $err;
-    //     } else {
-    //         echo $response;
-    //     }
-    // }
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+    }
 
     public function checkout_orders(Request $request)
     {
@@ -176,50 +176,50 @@ class HomeController extends Controller
 
         // $about = About::first();
         $orders = Order::where('id_member', Auth::guard('webmember')->user()->id)->latest()->first();
-        // $curl = curl_init();
+        $curl = curl_init();
 
-        // curl_setopt_array($curl, array(
-        //     CURLOPT_URL => "https://api.rajaongkir.com/starter/province",
-        //     CURLOPT_RETURNTRANSFER => true,
-        //     CURLOPT_ENCODING => "",
-        //     CURLOPT_MAXREDIRS => 10,
-        //     CURLOPT_TIMEOUT => 30,
-        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        //     CURLOPT_CUSTOMREQUEST => "GET",
-        //     CURLOPT_HTTPHEADER => array(
-        //         "key: 1adb8efa384245ff3bc71d4d2429518a"
-        //     ),
-        // ));
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.rajaongkir.com/starter/province",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "key: 1adb8efa384245ff3bc71d4d2429518a"
+            ),
+        ));
 
-        // $response = curl_exec($curl);
-        // $err = curl_error($curl);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
 
-        // curl_close($curl);
+        curl_close($curl);
 
-        // if ($err) {
-        //     echo "cURL Error #:" . $err;
-        // }
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        }
 
-        // $provinsi = json_decode($response);
+        $provinsi = json_decode($response);
         return view('home.checkout', compact('orders'));
     }
 
-    // public function payments(Request $request)
-    // {
-    //     Payment::create([
-    //         'id_order' => $request->id_order,
-    //         'id_member' => Auth::guard('webmember')->user()->id,
-    //         'jumlah' => $request->jumlah,
-    //         'provinsi' => $request->provinsi,
-    //         'kabupaten' => $request->kabupaten,
-    //         'kecamatan' => "",
-    //         'detail_alamat' => $request->detail_alamat,
-    //         'status' => 'Pending',
-    //         'no_rekening' => $request->no_rekening,
-    //         'atas_nama' => $request->atas_nama,
-    //     ]);
-    //     return redirect('/orders');
-    // }
+    public function payments(Request $request)
+    {
+        Payment::create([
+            'id_order' => $request->id_order,
+            'id_member' => Auth::guard('webmember')->user()->id,
+            'jumlah' => $request->jumlah,
+            'provinsi' => $request->provinsi,
+            'kabupaten' => $request->kabupaten,
+            'kecamatan' => "",
+            'detail_alamat' => $request->detail_alamat,
+            'status' => 'Pending',
+            'no_rekening' => $request->no_rekening,
+            'atas_nama' => $request->atas_nama,
+        ]);
+        return redirect('/orders');
+    }
 
     public function orders()
     {
